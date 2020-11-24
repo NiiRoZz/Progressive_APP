51a7f5c8e1c2b4552fd670d852d9497086935fb4
NiiRoZz <extazziii1000@gmail.com>
2020-11-24 10:12:15 +0100

    fetch

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -27,18 +27,34 @@ self.addEventListener('install', (e) => {
 	);
 });
 
-self.addEventListener('fetch', (e) => {
-    console.log('[Service Worker] Fetched resource '+e.request.url);
+/*self.addEventListener('fetch', (e) => {
+    console.log('[Service Worker] Fetched resource ' + e.request.url);
 	e.respondWith(
 		caches.match(e.request).then((r) => {
-			  console.log('[Service Worker] Fetching resource: '+e.request.url);
-		  return r || fetch(e.request).then((response) => {
-					return caches.open(cacheName).then((cache) => {
-			  console.log('[Service Worker] Caching new resource: '+e.request.url);
-			  cache.put(e.request, response.clone());
-			  return response;
+			console.log('[Service Worker] Fetching resource: '+e.request.url);
+			return r || fetch(e.request).then((response) => {
+				return caches.open(cacheName).then((cache) => {
+					console.log('[Service Worker] Caching new resource: '+e.request.url);
+					cache.put(e.request, response.clone());
+					return response;
+				});
 			});
-		  });
 		})
 	);
+});*/
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.open(cacheName).then(function (cache) {
+      return cache.match(event.request).then(function (response) {
+        return (
+          response ||
+          fetch(event.request).then(function (response) {
+            cache.put(event.request, response.clone());
+            return response;
+          })
+        );
+      });
+    }),
+  );
 });
