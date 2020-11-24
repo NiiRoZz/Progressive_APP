8c9cca79075443e6f2f643b8a35fdddc13782140
NiiRoZz <extazziii1000@gmail.com>
2020-11-24 10:17:21 +0100

    Push update

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -45,16 +45,8 @@ self.addEventListener('install', (e) => {
 
 self.addEventListener('fetch', function (event) {
   event.respondWith(
-    caches.open(cacheName).then(function (cache) {
-      return cache.match(event.request).then(function (response) {
-        return (
-          response ||
-          fetch(event.request).then(function (response) {
-            cache.put(event.request, response.clone());
-            return response;
-          })
-        );
-      });
+    fetch(event.request).catch(function () {
+      return caches.match(event.request);
     }),
   );
 });
