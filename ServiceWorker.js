21e12ad5ce282e145443b4af0042dd23ed747fdd
NiiRoZz <extazziii1000@gmail.com>
2020-11-24 10:43:18 +0100

    test .?

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -59,6 +59,7 @@ self.addEventListener('fetch', function (event) {
 		.catch(function () {
 		// If both fail, show a generic fallback:
 		console.log('[Service Worker] Resource fetch failed: '+event.request.url);
+		return caches.match('/index.html');
 		}),
 	);
 });
